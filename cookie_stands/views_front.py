54db3d03ceb211d2a0de172afc0c65a90529b886
Malik-Essa99 <malikhodrob99@gmail.com>
2023-08-07 11:35:42 +0000

    edited the code

--- cookie_stands/views_front.py
+++ cookie_stands/views_front.py
@@ -5,29 +5,30 @@ from .models import Cookie
 
 
 class CookieListView(LoginRequiredMixin, ListView):
-    template_name = "Cookies/Cookie_list.html"
+    template_name = "cookie_stands/Cookie_list.html"
     model = Cookie
     context_object_name = "Cookies"
 
 
 class CookieDetailView(LoginRequiredMixin, DetailView):
-    template_name = "Cookies/Cookie_detail.html"
+    template_name = "cookie_stands/Cookie_detail.html"
     model = Cookie
 
 
 class CookieUpdateView(LoginRequiredMixin, UpdateView):
-    template_name = "Cookies/Cookie_update.html"
+    template_name = "cookie_stands/Cookie_update.html"
     model = Cookie
     fields = "__all__"
 
 
 class CookieCreateView(LoginRequiredMixin, CreateView):
-    template_name = "Cookies/Cookie_create.html"
+    template_name = "cookie_stands/Cookie_create.html"
     model = Cookie
-    fields = ["name", "rating", "reviewer"] # "__all__" for all of them
+    # fields = ["", "", ""] # "__all__" for all of them
+    fields = "__all__" # "__all__" for all of them
 
 
 class CookieDeleteView(LoginRequiredMixin, DeleteView):
-    template_name = "Cookies/Cookie_delete.html"
+    template_name = "cookie_stands/Cookie_delete.html"
     model = Cookie
     success_url = reverse_lazy("Cookie_list")
